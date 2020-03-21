ff16c770b18cf9f8404f10430567ebca61670b65
Vonderland <897007204@qq.com>
2020-03-21 23:54:37 +0800

    preprocessing and tokenising the weibo data

---
+++ clean_data.py
@@ -0,0 +1,46 @@
+import re
+from tqdm import tqdm
+
+in_path = "./data/raw/weibo.tsv"
+out_path = "./data/clean/clean_weibo.tsv"
+
+remove = list('ノ¯︶ーσ･з∠~〜*&%$＊̀ ́；●ヾД≤≥ε┏゜ロ┛□▔﹏∇ψ❤三 ڡ ♂ㄒ;∂‸Ծˋ๑ºั`·ﾟゝ[]○▽￥←┴・｀.「\
+                    ﹃『』」∩ヽ ﾟ∀ｏ`´╭╮【Σっ★╥¬☆＜⌒ﾉ】→↑°╰╯┴x•ㅂ…|\/^<>口︵—≧≦⊙ω∑√')
+
+transmit = ["（转）", "（图转）", "「转」", "alink", "（转自网络）", "「图转」", "(转)", "(图片来自网络)"]
+
+
+def filter_emoji(desstr,restr=''):
+    try:
+        res = re.compile(u'[\U00010000-\U0010ffff]')
+    except re.error:
+        res = re.compile(u'[\uD800-\uDBFF][\uDC00-\uDFFF]')
+    return res.sub(restr, desstr)
+
+
+def handle_symbol(text):
+    for r in remove:
+        while r in text:
+            text = text.replace(r,"")
+    return text
+
+
+def filter_transmit(text):
+    for r in transmit:
+        while r in text:
+            text = text.replace(r, "")
+    return text
+
+with open(out_path, "w") as target:
+    with open(in_path, 'r') as f:
+        for line in tqdm(f):
+            try:
+                result = filter_transmit(line)
+                result = filter_emoji(result)
+                result = handle_symbol(result)
+                target.write(result)
+            except:
+                print("exception")
+                continue
+f.close()
+target.close()
---
+++ tokenize_data.py
@@ -0,0 +1,44 @@
+from transformers import BertTokenizer
+from tqdm import tqdm
+
+PAD = '[PAD]'
+n_ctx = 300
+vocab_path = "./vocabulary/vocab_small.txt"
+train_path = "./data/clean/clean_weibo.tsv"
+tokenized_path = "./data/train_tokenized.txt"
+
+tokenizer = BertTokenizer(vocab_file=vocab_path)
+vocab_size = len(tokenizer)
+
+pad_id = tokenizer.convert_tokens_to_ids(PAD)
+print("pad_id = ", pad_id)
+
+print("tokenizing raw data,raw data path:{}, token output path:{}".format(train_path, tokenized_path))
+with open(train_path, 'rb') as raw_f:
+    data = raw_f.read().decode("utf-8")
+raw_f.close()
+train_data = data.split("\n")
+print("there are {} dialogue in raw dataset".format(len(train_data)))
+
+with open(tokenized_path, "w", encoding="utf-8") as f:
+    for dialogue_index, dialogue in enumerate(tqdm(train_data)):
+        utterances = dialogue.split("\t")
+        # each dialog begins with [CLS]
+        dialogue_ids = [tokenizer.cls_token_id]
+        for utterance in utterances:
+            dialogue_ids.extend([tokenizer.convert_tokens_to_ids(word) for word in utterance])
+            # each utterance ends with [SEP], standing for ending
+            dialogue_ids.append(tokenizer.sep_token_id)
+        # limit the length of each dialog
+        dialogue_ids = dialogue_ids[:n_ctx]
+        for dialogue_id in dialogue_ids:
+            f.write(str(dialogue_id) + ' ')
+        if dialogue_index < len(train_data) - 1:
+            f.write("\n")
+f.close()
+
+
+
+
+
+
